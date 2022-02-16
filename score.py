e004c05f2a703e05e07edebcfcb8451143402067
RobertSmithers <rjsmithers3@gmail.com>
2022-02-15 21:10:38 -0500

    Full project

--- score.py
+++ score.py
@@ -1,34 +1,40 @@
+import requests
+import json
 import pandas as pd
 import numpy as np
-from sklearn.metrics import mean_absolute_error
+from sklearn.metrics import mean_absolute_error as MAE
 from rdkit import Chem
 import mordred
 from mordred import Calculator, descriptors
 
-score = 0.0
+scoreboard = 'ffirhduscve'
 
-def smiles_to_mol(smiles):
-    m = Chem.MolFromSmiles(smiles)
-    m = Chem.AddHs(m)
-    return m
+get = 'https://keepthescore.co/api/' + scoreboard + '/board'
+post = 'https://keepthescore.co/api/' + scoreboard + '/add_single_score'
 
-def featurize(dataset,features):
-    df = pd.read_csv(dataset)
-    y_test = np.array(df['Solubility'])
-    calc = mordred.Calculator()
-    calc.register(features)
-    df = df['SMILES'].apply(smiles_to_mol)
-    X_test = np.array(calc.pandas(df))
-    return X_test, y_test
+def report(teamname,score):
+    test_error = score
+    r = requests.get(get)
+    scores = json.loads(r.text)
+    for entry in scores['players']:
+        if entry['player_name'] == teamname:
+            diff = test_error - entry['score']
+            break
+    if (diff < 0):
+        commit(diff,teamname)
+    else:
+        print('Your score is worse than your previous best score, it will not be reported.')
 
-def test(features,model):
-    global done, score
-    X_test, y_test = featurize('../../Data/Solubility/dataset-E.csv',features)
-    y_pred = model.predict(X_test)
-    mae = mean_absolute_error(y_test, y_pred)
-    if score < 0:
-        print('Your score is negative. Something is wrong.')
-        return
-    done = True
-    score = mae
-    return mae
+def commit(diff,teamname):
+    requests.post(post, json={ "player_name": teamname, "score": diff, "comment": "test" })
+
+def test(predictor,teamname):
+    data_E_E0 = pd.read_csv('dataset-E-E0.csv')
+    E_X = np.array(data_E_E0['SMILES'])
+    E_E0 = np.array(data_E_E0['E0'])
+    y_pred = np.zeros(E_E0.shape)
+    for i in range(len(E_X)):
+        y_pred[i] = predictor(E_X[i])[0]
+    mae = MAE(y_pred,E_E0) * 23.06
+    report(teamname,mae)
+    return mae 
